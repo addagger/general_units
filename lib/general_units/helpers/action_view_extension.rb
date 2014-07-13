c3c574864a3cfa920a6a04db48028bf10319ac6e
macintosh <macintosh@nyc.local>
2014-07-13 22:32:30 +0400

    short_weight_unit_for_select

--- lib/general_units/helpers/action_view_extension.rb
+++ lib/general_units/helpers/action_view_extension.rb
@@ -5,13 +5,21 @@ module GeneralUnits
   
     included do
     end
-    
+
     def weight_units_for_select
       Weight.units.map {|u| [u.name, u.code]}
     end
-    
+        
+    def short_weight_units_for_select
+      Weight.units.map {|u| [u.short, u.code]}
+    end
+
     def length_units_for_select
       Length.units.map {|u| [u.name, u.code]}
+    end
+        
+    def short_length_units_for_select
+      Length.units.map {|u| [u.short, u.code]}
     end
     
   end
