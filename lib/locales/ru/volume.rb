cc327e7a2fb2acfb10f3cbec1327d82440a5d066
macintosh <macintosh@nyc.local>
2014-07-24 05:19:52 +0400

    locales added

--- lib/locales/ru/volume.rb
+++ lib/locales/ru/volume.rb
@@ -15,15 +15,15 @@
             :cubic_millimeter => {:zero => "Cubic Millimeter", :one => "Cubic Millimeter", :two => "Cubic Millimeter", :few => "Cubic Millimeter", :many => "Cubic Millimeter", :other => "Cubic Millimeter"}
           },
           :short => {
-            :mile_nautical=> {:zero => "mln<sup>3</sup>", :one => "mln<sup>3</sup>", :two => "mln<sup>3</sup>", :few => "mln<sup>3</sup>", :many => "mln<sup>3</sup>", :other => "mln<sup>3</sup>"},
-            :mile => {:zero => "ml<sup>3</sup>", :one => "ml<sup>3</sup>", :two => "ml<sup>3</sup>", :few => "ml<sup>3</sup>", :many => "ml<sup>3</sup>", :other => "ml<sup>3</sup>"},
-            :yard => {:zero => "yrd<sup>3</sup>", :one => "yrd<sup>3</sup>", :two => "yrd<sup>3</sup>", :few => "yrd<sup>3</sup>", :many => "yrd<sup>3</sup>", :other => "yrd<sup>3</sup>"},
-            :foot => {:zero => "ft<sup>3</sup>", :one => "ft<sup>3</sup>", :two => "ft<sup>3</sup>", :few => "ft<sup>3</sup>", :many => "ft<sup>3</sup>", :other => "ft<sup>3</sup>"},
-            :inch => {:zero => "in<sup>3</sup>", :one => "in<sup>3</sup>", :two => "in<sup>3</sup>", :few => "in<sup>3</sup>", :many => "in<sup>3</sup>", :other => "in<sup>3</sup>"},
-            :kilometer => {:zero => "km<sup>3</sup>", :one => "km<sup>3</sup>", :two => "km<sup>3</sup>", :few => "km<sup>3</sup>", :many => "km<sup>3</sup>", :other => "km<sup>3</sup>"},
-            :meter => {:zero => "m<sup>3</sup>", :one => "m<sup>3</sup>", :two => "m<sup>3</sup>", :few => "m<sup>3</sup>", :many => "m<sup>3</sup>", :other => "m<sup>3</sup>"},
-            :centimeter => {:zero => "cm<sup>3</sup>", :one => "cm<sup>3</sup>", :two => "cm<sup>3</sup>", :few => "cm<sup>3</sup>", :many => "cm<sup>3</sup>", :other => "cm<sup>3</sup>"},
-            :millimeter => {:zero => "mm<sup>3</sup>", :one => "mm<sup>3</sup>", :two => "mm<sup>3</sup>", :few => "mm<sup>3</sup>", :many => "mm<sup>3</sup>", :other => "mm<sup>3</sup>"}
+            :cubic_mile_nautical=> {:zero => "mln<sup>3</sup>", :one => "mln<sup>3</sup>", :two => "mln<sup>3</sup>", :few => "mln<sup>3</sup>", :many => "mln<sup>3</sup>", :other => "mln<sup>3</sup>"},
+            :cubic_mile => {:zero => "ml<sup>3</sup>", :one => "ml<sup>3</sup>", :two => "ml<sup>3</sup>", :few => "ml<sup>3</sup>", :many => "ml<sup>3</sup>", :other => "ml<sup>3</sup>"},
+            :cubic_yard => {:zero => "yrd<sup>3</sup>", :one => "yrd<sup>3</sup>", :two => "yrd<sup>3</sup>", :few => "yrd<sup>3</sup>", :many => "yrd<sup>3</sup>", :other => "yrd<sup>3</sup>"},
+            :cubic_foot => {:zero => "ft<sup>3</sup>", :one => "ft<sup>3</sup>", :two => "ft<sup>3</sup>", :few => "ft<sup>3</sup>", :many => "ft<sup>3</sup>", :other => "ft<sup>3</sup>"},
+            :cubic_inch => {:zero => "in<sup>3</sup>", :one => "in<sup>3</sup>", :two => "in<sup>3</sup>", :few => "in<sup>3</sup>", :many => "in<sup>3</sup>", :other => "in<sup>3</sup>"},
+            :cubic_kilometer => {:zero => "km<sup>3</sup>", :one => "km<sup>3</sup>", :two => "km<sup>3</sup>", :few => "km<sup>3</sup>", :many => "km<sup>3</sup>", :other => "km<sup>3</sup>"},
+            :cubic_meter => {:zero => "m<sup>3</sup>", :one => "m<sup>3</sup>", :two => "m<sup>3</sup>", :few => "m<sup>3</sup>", :many => "m<sup>3</sup>", :other => "m<sup>3</sup>"},
+            :cubic_centimeter => {:zero => "cm<sup>3</sup>", :one => "cm<sup>3</sup>", :two => "cm<sup>3</sup>", :few => "cm<sup>3</sup>", :many => "cm<sup>3</sup>", :other => "cm<sup>3</sup>"},
+            :cubic_millimeter => {:zero => "mm<sup>3</sup>", :one => "mm<sup>3</sup>", :two => "mm<sup>3</sup>", :few => "mm<sup>3</sup>", :many => "mm<sup>3</sup>", :other => "mm<sup>3</sup>"}
           }
         }
       }
