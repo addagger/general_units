5bd44d41e21ab557de9fbe4d5e81c9cd7e30b1ca
macintosh <macintosh@nyc.local>
2014-07-24 04:34:15 +0400

    locales added

--- lib/general_units/railtie.rb
+++ lib/general_units/railtie.rb
@@ -2,7 +2,7 @@ require 'rails'
 
 module GeneralUnits
   class Railtie < ::Rails::Railtie
-    config.i18n.load_path += Dir[ File.join(File.dirname(__FILE__), 'locales', '**', '*.{rb,yml}') ]
+    # config.i18n.load_path += Dir[ File.join(File.dirname(__FILE__), 'locales', '**', '*.{rb,yml}') ]
     config.before_initialize do
       ActiveSupport.on_load :active_record do
         require 'general_units/models/active_record_extension'
